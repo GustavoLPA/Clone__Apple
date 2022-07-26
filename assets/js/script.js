f820c88ab4cadeaffb3c5fc8f6e855dcfb672f43
Gustavo Lopes <gustavoalmeida3858@gmail.com>
2022-07-26 13:40:16 -0300

    Inicio responsividade | Navbar mobile | Footer mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// Navbar mobile e desktop
 var IconOpen = document.querySelector(".IconOpen");
 var IconClose = document.querySelector(".IconClose");
 var IconBag = document.querySelector(".IconBag");
@@ -9,13 +10,20 @@ IconOpen.addEventListener("click", function () {
   header.style.transition = "0.5s";
   header.style.background = "#000000";
   menu.style.height = "100vh";
-
   IconOpen.style.display = "none";
   IconClose.style.display = "block";
-
+  IconBag.style.visibility = "hidden";
   document.documentElement.style.overflow = "hidden";
-});
 
+  document.body.onresize = function () {
+    if (document.body.clientWidth > 768) {
+      menu.style.height = "0";
+      IconOpen.style.display = "block";
+      IconClose.style.display = "none";
+      header.style.background = "#000000cc";
+    }
+  };
+});
 
 IconClose.addEventListener("click", function () {
   let menu = document.querySelector(".mobile__list");
@@ -23,9 +31,10 @@ IconClose.addEventListener("click", function () {
   header.style.background = "#000000cc";
   header.style.transition = "1.4s";
   menu.style.height = "0";
-
   IconOpen.style.display = "block";
   IconClose.style.display = "none";
-
+  IconBag.style.visibility = "visible";
   document.documentElement.style.overflow = "auto";
 });
+
+// Menu de navegação footer
